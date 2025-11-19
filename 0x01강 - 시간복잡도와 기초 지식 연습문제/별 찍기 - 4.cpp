fe1064683df23132283a4c05e1f2456d9ad15806
moaventurer <lalae0228@gmail.com>
2025-11-19 20:57:54 +0900

    2025-11-19 7문제

---
+++ 0x01강 - 시간복잡도와 기초 지식 연습문제/별 찍기 - 4.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    char* arr1 = new char[N+1];
+    char* arr2 = new char[N+1];
+    for(int i=0; i<N; i++){
+        arr1[i] = '*';
+        arr2[i] = ' ';
+    }
+    for(int i=N; i>0; i--){
+        cout.write(arr2, N-i);
+        cout.write(arr1, i);
+        cout << '\n';
+    }
+    delete[] arr1;
+    delete[] arr2;
+    return 0;
+}
---
+++ 0x01강 - 시간복잡도와 기초 지식 연습문제/별 찍기 - 6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+    char* arr1 = new char[2*N+1];
+    char* arr2 = new char[2*N+1];
+    for(int i=0; i<2*N; i++){
+        arr1[i] = '*';
+        arr2[i] = ' ';
+    }
+    for(int i=1; i<=N; i++){
+        cout.write(arr2, i-1);
+        cout.write(arr1, (N-i)*2 + 1);
+        cout << '\n';
+    }
+    delete[] arr1;
+    delete[] arr2;
+    return 0;
+}
